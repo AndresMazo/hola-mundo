f78c062ab343abb6da8dd5eac4ff09134fddb8bc
AndresMazo <112454128+AndresMazo@users.noreply.github.com>
2022-08-29 21:58:50 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        . # # # .
+        . # # # .
+        . # # # .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(200)
+        basic.showIcon(IconNames.Heart)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # # # .
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.clearScreen()
+})
+basic.showString("Hola Mundo")
